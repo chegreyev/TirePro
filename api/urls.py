b90d2d4b0d708fcdecbe053ceb60656dde30bb95
chegreyev <chegreyev@gmail.com>
2020-05-08 11:13:55 +0500

    connected views

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
 from rest_framework.routers import DefaultRouter
+from .views import *
 
 router = DefaultRouter
-router.register()
+router.register('producers', ProducerViewSet, basename='Изготовитель')
+router.register('tires', TireViewSet, basename='Шины')
 
 
 urlpatterns = router.urls
