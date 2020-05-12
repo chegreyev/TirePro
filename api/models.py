75daff304c8dfb8c7cd5fbe691dda11bed419b09
chegreyev <chegreyev@gmail.com>
2020-05-12 06:10:29 +0500

    ADD: car model

--- api/models.py
+++ api/models.py
@@ -14,11 +14,11 @@ class Tire(Model):
     profile = FloatField(default=0)
     diameter = CharField(max_length=20, blank=True)
 
-    seoson = CharField(max_length=6, choices=SEOSON, default="L")
+    seoson = CharField(max_length=11, choices=SEOSON, default="L")
     producer = ForeignKey(Producer, on_delete=CASCADE)
 
     def __str__(self):
-        return self.producer
+        return self.producer.name
 
 class Car(Model):
     mark = CharField(max_length=150)
@@ -26,6 +26,8 @@ class Car(Model):
     year = IntegerField()
     modification = CharField(max_length=100)
     type_size = CharField(max_length=100)
+
+    tire = ForeignKey(Tire, on_delete=CASCADE)
 
     def __str__(self):
         return f'{self.mark}-{self.model}-{self.year}-{self.modification}-{self.type_size}'
