96676be626cfe28f991f57c1ab45cb2716724edf
chegreyev <chegreyev@gmail.com>
2020-05-21 03:01:15 +0500

    some changes after merge

--- TirePro/urls.py
+++ TirePro/urls.py
@@ -19,13 +19,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-<<<<<<< HEAD
     path(r'admin/', admin.site.urls),
     path(r'api/v1/', include('api.urls')),
     path(r'auth/', include('emailsignup.urls')),
-]
-=======
-    path('admin/', admin.site.urls),
-    path('api/v1/', include('api.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
->>>>>>> zhaslan
