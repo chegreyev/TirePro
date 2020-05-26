63fb768c055c58bd28299899bbcd128c25e4b3ee
chegreyev <chegreyev@gmail.com>
2020-05-27 02:54:40 +0500

    changed email for the .env email

--- src/emailsignup/functions.py
+++ src/emailsignup/functions.py
@@ -1,12 +1,15 @@
 from django.contrib.auth import get_user_model
 from django.core.mail import send_mail
-from django.template.loader import render_to_string
+from dotenv import load_dotenv
+
+import os
 
 User = get_user_model()
+load_dotenv()
 
 def email_client(request):
     username = request.POST.get('username')
     client = User.objects.get(username=username)
     # msg_html = render_to_string('templates/account/email/feedback.txt', {'client': client})
     template_email_text = ''
-    return send_mail('Lelander work samples', template_email_text, 'chegreyev@gmail.com', [f'{client.email}'],  fail_silently=False)
+    return send_mail('Thank you for the feedback , we really appreciate it.', template_email_text, f'{os.getenv("EMAIL_HOST_USER")}', [f'{client.email}'],  fail_silently=False)
