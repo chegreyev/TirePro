9adc8637b85553b202403b5594b1fcf9392c1e4a
chegreyev <chegreyev@gmail.com>
2020-05-28 03:07:26 +0500

    Fix: some changes

--- src/api/views.py
+++ src/api/views.py
@@ -1,5 +1,4 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.response import Response
 from .serializers import *
 
 class ProducerViewSet(ModelViewSet):
@@ -11,20 +10,11 @@ class TireViewSet(ModelViewSet):
     queryset = Tire.objects.all()
     serializer_class = TireSerializer
 
-    def list(self, request, *args, **kwargs):
-
-        page = self.paginate_queryset(self.get_queryset())
-
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
-
-        serializer = self.get_serializer(self.get_queryset(), many = True)
-        return Response(serializer.data)
 
 class CarViewSet(ModelViewSet):
     queryset = Car.objects.all()
     serializer_class = CarSerializer
+
 
 class DiscViewSet(ModelViewSet):
     queryset = Disc.objects.all()
